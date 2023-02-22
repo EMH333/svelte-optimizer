7656939d3f91f77354c670a87f04d4d2e11b2b63
Ethan Hampton <etho@ethohampton.com>
2023-02-21 20:17:37 -0800

    Fix preprocessTest script

--- preprocessTest.js
+++ preprocessTest.js
@@ -1,6 +1,9 @@
 import { readFileSync } from "fs";
+import { resolve } from "path";
+import { cwd } from "process";
 import { preprocess } from "svelte/compiler";
 import preprocessor from "./preprocessor.js";
+import { scanDir } from "./scan.js";
 
 //get filename from command line
 const filename = process.argv[2];
@@ -8,12 +11,13 @@ const filename = process.argv[2];
 //read file
 const file = readFileSync(filename, "utf8");
 
-const usedExternal = new Set();
-usedExternal.add("test");
-usedExternal.add("items");
-usedExternal.add("selectedItem");
+//TODO get this working with the new scanDir function
+//get directory file is in
+const dir = resolve(cwd(), filename).substring(0, resolve(cwd(), filename).lastIndexOf("/"));
+
+const usedExternal = await scanDir(dir);
 
 //run svelte preprocess on it
-const { code } = await preprocess(file, [preprocessor(usedExternal)]);
+const { code } = await preprocess(file, [preprocessor(usedExternal)], { filename });
 
 console.log("\n\nCode:", code);
